d2f09f2445221b7f53998a31f2b1bbd1ed382b1e
ianttta <iantastream@gmail.com>
2023-10-20 09:10:49 -0700

    Main Screen UI Completed Here

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:dart_ping/dart_ping.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:latencytool/ping_results_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -24,28 +21,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> ipAddresses = [];
-  List<String> pingResults = [];
+  List<String> buttonNames = [
+    '15 sec',
+    '30 sec',
+    '60 sec',
+    '90 sec',
+    '120 sec',
+  ];
 
-  void startPing() async {
-    for (final ipAddress in ipAddresses) {
-      final ping = Ping(ipAddress, count: 5);
+  List<String> buttonNames2 = [
+    '30 min',
+    '1 hr',
+    '3 hr',
+    '6 hr',
+    '12 hr',
+    '24 hr',
+    '1 W',
+    '2 W',
+    '1 M',
+  ];
 
-      await for (final event in ping.stream) {
-        if (kDebugMode) {
-          print(event);
-        } // Optional: Print the ping results for debugging
-        pingResults.add(event as String);
-      }
-    }
-
-    // Navigate to the PingResultsScreen and pass the ping results
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => PingResultsScreen(pingResults),
-      ),
-    );
-  }
+  // List to store IP addresses
+  List<String> ipAddresses = List.filled(12, ""); // Initialize with 10 empty strings
 
   @override
   Widget build(BuildContext context) {
@@ -58,6 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            // Left column for IP input
             Flexible(
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
@@ -72,7 +70,8 @@ class _MyHomePageState extends State<MyHomePage> {
                             hintText: "Enter IP ${index + 1}",
                           ),
                           onChanged: (text) {
-                            ipAddresses.add(text);
+                            // Update the IP address in the list
+                            ipAddresses[index] = text;
                           },
                         ),
                     ],
@@ -81,11 +80,13 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
 
+            // Vertical separator line
             Container(
               width: 0.5,
               color: Colors.black,
             ),
 
+            // Right column for the rest of the UI
             Flexible(
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
@@ -95,15 +96,96 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        // Rest of your UI components
-                        // ...
+                        const Text(
+                          "Intervals",
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 15.0),
+
+                        // Rest of your UI components can be added here
+                        Center(
+                          child: Wrap(
+                            alignment: WrapAlignment.center,
+                            spacing: 10.0,
+                            runSpacing: 15.0,
+                            children: [
+                              for (int i = 0; i < buttonNames.length; i++)
+                                ElevatedButton(
+                                  onPressed: () {
+                                    // Add button functionality here.
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
+                                    minimumSize: MaterialStateProperty.all(
+                                      const Size(
+                                        100,
+                                        50,
+                                      ),
+                                    ),
+                                  ),
+                                  child: Text(buttonNames[i]),
+                                ),
+                            ],
+                          ),
+                        ),
+
+                        const SizedBox(height: 15.0),
+                        const Divider(
+                          height: 20,
+                          color: Colors.blueGrey,
+                        ),
+                        const SizedBox(height: 5.0),
+
+                        const Text(
+                          "Time",
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 15.0),
+
+                        Center(
+                          child: Wrap(
+                            alignment: WrapAlignment.center,
+                            spacing: 10.0,
+                            runSpacing: 15.0,
+                            children: [
+                              for (int i = 0; i < buttonNames2.length; i++)
+                                ElevatedButton(
+                                  onPressed: () {
+                                    // Add button functionality here.
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(Colors.indigoAccent),
+                                    minimumSize: MaterialStateProperty.all(
+                                      const Size(
+                                        100,
+                                        50,
+                                      ),
+                                    ),
+                                  ),
+                                  child: Text(buttonNames2[i]),
+                                ),
+                            ],
+                          ),
+                        ),
+
+                        const SizedBox(height: 10.0),
+                        const Divider(
+                          height: 20,
+                          color: Colors.blueGrey,
+                        ),
+                        const SizedBox(height: 10.0),
 
                         ElevatedButton(
-                          onPressed: startPing,
+                          onPressed: () {
+                            // Add button functionality here.
+                          },
                           style: ButtonStyle(
                             backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
                             minimumSize: MaterialStateProperty.all(
-                              const Size(200, 50),
+                              const Size(
+                                200,
+                                50,
+                              ),
                             ),
                           ),
                           child: const Text("Start Tool"),
